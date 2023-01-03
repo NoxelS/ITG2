071d8993f01770b7893db9ca50d741d3eb29bb29
Noel Pascal Schwabenland <noel@schwabenland.ch>
2023-01-03 14:18:50 +0100

    fixed U9-225

--- U9-225/src/main.cpp
+++ U9-225/src/main.cpp
@@ -1,12 +1,11 @@
 /******************************************************************/
-#define SLOW_DOWN       // insert additional sleeps to slow down speed
-#define XMAX 1800        // screen width
-#define YMAX 315        // screen height
-#define MAX_ITER 10000  // maximale Anzahl Iterationen
+#define XMAX 70        // screen width
+#define YMAX 30        // screen height
 #define X0 -2.1         // window parameters
 #define X1 0.8
 #define Y0 -1.4
-#define Y1 1.4
+#define Y1 1.46
+#define MAX_ITER 1000  // max iterations
 /******************************************************************/
 
 #include <unistd.h>
@@ -19,11 +18,9 @@
 #include <mutex>
 #include <thread>
 #include <vector>
+#include <atomic>
 using namespace std;
 
-////////////////////
-// complex-Klasse //
-////////////////////
 struct complex {
     double re, im;
     complex() : complex(0, 0) {}
@@ -34,9 +31,6 @@ struct complex {
     double abs() { return sqrt(re * re + im * im); }
 };
 
-//////////////////////////
-// Mandelbort-Iteration //
-//////////////////////////
 int board[XMAX][YMAX];
 mutex board_mtx;
 
@@ -45,13 +39,12 @@ struct iterate {
         complex z = complex();
         complex c(X0 + i * dx, Y0 + j * dy);
 
+        // Calculate z
         for (int n = 0; n < 100 && z.abs() < 10; ++n) z = z * z + c;
-        // usleep(10000);
+
         board_mtx.lock();
         board[i][j] = z.abs();
         board_mtx.unlock();
-
-        // cout << i << ", " << j << "TASK: ";
     }
     double dx = (X1 - X0) / XMAX;
     double dy = (Y1 - Y0) / YMAX;
@@ -59,23 +52,17 @@ struct iterate {
     int j;
 };
 
-////////////////
-// Task-Queue //
-////////////////
 deque<iterate> TaskQueue;  // Container mit Funktor-Elementen
 mutex TaskQueue_mtx;
-int done_tasks = 0;  // abgearbeitete Tasks
+atomic<int> done_tasks = 0;  // abgearbeitete Tasks
 
-/////////////////
-// Thread-Pool //
-/////////////////
 const int pool_length = thread::hardware_concurrency();
 vector<thread> ThreadPool;
 vector<bool> TP_activity;
 mutex TP_activity_mtx;
 
 void wait_loop(int thread_id) {
-    while (done_tasks < XMAX * YMAX) {
+    while (done_tasks < YMAX * XMAX) {
         TaskQueue_mtx.lock();
         if (TaskQueue.empty()) {
             TaskQueue_mtx.unlock();
@@ -88,7 +75,7 @@ void wait_loop(int thread_id) {
         // Unlock the mutex so other tasks can look for tasks
         TaskQueue_mtx.unlock();
 
-        // Do stuff
+        // Do calculations
         TP_activity_mtx.lock();
         TP_activity[thread_id] = true;
         TP_activity_mtx.unlock();
@@ -102,16 +89,12 @@ void wait_loop(int thread_id) {
     }
 };
 
-/////////////
-// Ausgabe //
-/////////////
 void print_board() {
     ofstream myfile;
-    while (true) {
-        myfile.open("mandelbrot");
-        // system("clear");
-        // cout << "\n\n\n\n\n\n\n\n\n   ";
+    while (done_tasks < YMAX * XMAX) {
+        myfile.open("mandelbrot.txt");
 
+        myfile << "   ";
         for (int i = 0; i < XMAX; ++i) myfile << '_';
         myfile << '\n';
 
@@ -141,26 +124,26 @@ void print_board() {
 
         // active threads in pool
         int act_tasks = 0;
-        myfile << "   act: [";
-        for (int i = 0; i < pool_length; ++i) myfile << (TP_activity[i] ? ++act_tasks, '+' : '.');
-        cout << "] act/tot = " << setw(2) << act_tasks << "/" << done_tasks << endl;
+        cout << "act/tot = " << setw(2) << act_tasks << "/" << done_tasks << endl;
 
         myfile.close();
+
         sleep(1);
     }
+    cout << "Done. Saved the mandelbrot to file" << endl;
 }
 
 int main() {
-    // Ausgabe
+    // Monitor threads
     thread monitor(print_board);
 
     // Fill Task Queue
     for (int j = 0; j < YMAX; ++j) {
         for (int i = 0; i < XMAX; ++i) {
-            TaskQueue_mtx.lock();
             iterate iter = iterate();
             iter.i = i;
             iter.j = j;
+            TaskQueue_mtx.lock();
             TaskQueue.push_front(iter);
             TaskQueue_mtx.unlock();
         }
@@ -172,7 +155,7 @@ int main() {
         ThreadPool.emplace_back(wait_loop, id);
     }
 
-    for (auto& th : ThreadPool) th.detach();
+    for (auto& th : ThreadPool) th.join();
 
     monitor.join();
 }
